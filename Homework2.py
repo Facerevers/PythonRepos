b1199f129fbc41ffb4fadf6a78694aee9f991d58
crazle <crazle@mail.ru>
2023-09-13 11:16:38 +0300

    Task12 code completed

--- Homework2.py
+++ Homework2.py
@@ -1,16 +1,5 @@
-import random
-coins = int(input('Введите количество монеток: '))
-rand_list = []
-for i in range(coins):
-    rand_list.append(random.randint(0, 1))
-print(rand_list)
-countofheads, countoftails = 0, 0
-for i in rand_list:
-    if i == 1:
-        countofheads += 1
-    elif i == 0:
-        countoftails += 1
-if countofheads >= countoftails:
-    print(countoftails)
-else:
-    print(countofheads)
+summa = int(input('Введите сумму двух чисел: '))
+product = int(input('Введите произведение двух чисел: '))
+firstnum = ((-1 * summa) - ((summa ** 2) - (4 * -1 * (-1 * product))) ** 0.5)/ -2
+secondnum = summa - firstnum
+print(firstnum, secondnum)
