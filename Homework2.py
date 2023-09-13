eedf4592c4cbae92512739117767c8c9706b2b66
crazle <crazle@mail.ru>
2023-09-13 11:17:15 +0300

    Task14 code completed

--- Homework2.py
+++ Homework2.py
@@ -1,5 +1,7 @@
-summa = int(input('Введите сумму двух чисел: '))
-product = int(input('Введите произведение двух чисел: '))
-firstnum = ((-1 * summa) - ((summa ** 2) - (4 * -1 * (-1 * product))) ** 0.5)/ -2
-secondnum = summa - firstnum
-print(firstnum, secondnum)
+n = int(input('Введите число N: '))
+i = 1
+lst = []
+while i <= n:
+    lst.append(i)
+    i *= 2
+print(lst)
