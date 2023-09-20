e539ff0d3ed4ab9bba38d3e21749e5cf86652d47
crazle <crazle@mail.ru>
2023-09-20 23:51:51 +0300

    Task 22 redacted with using map

--- Task22.py
+++ Task22.py
@@ -3,18 +3,8 @@
 Пользователь вводит 2 числа. n — кол-во элементов первого множества. m — кол-во элементов второго множества. 
 Затем пользователь вводит сами элементы множеств."""
 
-n = int(input(' Введите количество элементов первого множества: '))
-m = int(input(' Введите количество элементов второго множества: '))
-list_1 = input('Введите элементы первого множества через пробел: ')
-list_2 = input('Введите элементы второго множества через пробел: ')
-lst1 = list_1.split()
-lst2 = list_2.split()
-first_mn = []
-second_mn = []
-for i in lst1:
-    first_mn.append(i)
-for i in lst2:
-    second_mn.append(i)
+first_mn = list(map(int, input("Введите элементы первого множества: ").split(" ")))
+second_mn = list(map(int, input("Введите элементы второго множества: ").split(" ")))
 fst = set(first_mn)
 scnd = set(second_mn)
 out_mn = fst.intersection(scnd)
